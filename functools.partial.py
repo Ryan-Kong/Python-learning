d3dc4e23c7986b0e6ad0b3046fc2fb379597fa4d
kongxr@foxmail.com <mac@MacdeiMac.local>
2018-02-20 17:25:51 +0800

    偏函数：快速固定参数

---
+++ functools.partial.py
@@ -0,0 +1,5 @@
+# _*_ coding: utf_8 _*_
+import functools
+int2 = functools.partial(int, base = 2)     # functools.partial 把一个函数的某些参数给固定住（也就是设置默认值），返回一个新的函数，调用这个新函数会更简单。
+a = int2('1000000000')
+print(a)
