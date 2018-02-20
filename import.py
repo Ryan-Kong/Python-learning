47af9fbbbc730cdd8275229e5db409fee951ca25
kongxr@foxmail.com <mac@MacdeiMac.local>
2018-02-20 18:04:21 +0800

    导入模块的例子

---
+++ import.py
@@ -0,0 +1,13 @@
+#! /usr/local/bin/python3
+# _*_ coding: utf-8 _*_
+import sys
+def test():
+    args = sys.argv
+    if len(args)==1:
+        print('Hello World!')
+    elif len(args)==2:
+        print('Hello, %s!' % args[1])
+    else:
+        print('Too many arguments!')
+if __name__ = '__main__':
+    test()
