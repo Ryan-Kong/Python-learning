780d8a381bc130277d491b03ef8b88b3d4ec563d
iMac <kongxr@foxmail.com>
2018-02-26 00:26:31 +0800

    day 7: Python way to set configurations

---
+++ awesome-python-webapp/www/config.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+'''
+Configuration
+'''
+
+import config_default.py
+
+class Dict(dict):
+    '''
+    Simple dict but support access as x.y style.
+    '''
+    def __init__(self, names = (), value = (), **kw):
+        super(Dict, self).__init__(**kw)
+        for k, v in zip(names, values):
+            self[k] = v
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key )
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def merge(defaults, override):
+        r = {}
+        for k, v in defaults.item():
+            if k in override:
+                if isinstance(v, dict):
+                    r[k] = merge(v, override[k])
+                else:
+                    k[k] = v
+            return r
+
+    def toDict(d):
+        D = Dict()
+        for k, v in d.item():
+            D[k] = toDict(v) if isinstance(v, dict) else v
+        return D
+
+configs =  config_default.configs
+
+try:
+    import config_override
+    configs = merge(configs, config_override.configs)
+except ImportError:
+    pass
+
+configs = toDict(configs)
+
+
---
+++ awesome-python-webapp/www/config_default.py
@@ -0,0 +1,19 @@
+# -*- conding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+
+config = {
+    'debug': True,
+    'db': {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'www'
+    'password': 'www',
+    'db': 'awesome'
+    }
+    'session':{
+        'secret': 'Awesome'
+    }
+}
